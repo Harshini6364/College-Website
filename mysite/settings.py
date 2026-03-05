f4ad06ca9eee44cb8f068502f31f8bc148dc9085
Harshini <24b01a1223@svecw.edu.in>
2026-03-05 09:54:37 +0530

    Fix database config to use MySQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ if os.environ.get('MYSQLHOST'):
             'USER': os.environ.get('MYSQLUSER'),
             'PASSWORD': os.environ.get('MYSQLPASSWORD'),
             'HOST': os.environ.get('MYSQLHOST'),
-            'PORT': os.environ['MYSQLPORT'],
+            'PORT': os.environ.get('MYSQLPORT'),
         }
     }
 else:
